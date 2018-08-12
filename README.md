e00c4e7f4aec419d4c8d0a42f974f15c536f5e2d
siddhantk232 <siddhantk232@gmail.com>
2018-08-12 16:52:12 +0530

    final release

---
+++ README.md
@@ -0,0 +1,16 @@
+# Welcome
+
+##### This is a chat app made with socket.io
+
+---
+
+Follow the steps below to run this code on your machine:
+
+```console
+    git clone https://github.com/siddhantk232/u-chat.git
+    cd u-chat
+    npm install
+    node ./app.js
+```
+
+#### I will also make a chat app with better UI in future :-)
---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+
+server = app.listen(3000, () => {
+  console.log(`Server started on port 3000 `);
+});
+
+const io = require("socket.io")(server);
+
+//Listen on every connection
+io.on("connection", socket => {
+  //default username
+  socket.username = "anonymous";
+
+  socket.on("change_username", data => {
+    socket.username = data.username;
+  });
+
+  socket.on("new_message", data => {
+    io.sockets.emit("new_message", {
+      message: data.message,
+      username: socket.username
+    });
+  });
+});
